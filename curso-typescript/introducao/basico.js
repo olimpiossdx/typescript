a72646c68fb2fac1c90a844f30c0164ac090deb0
Olimpio Pimenta <olimpiodx@gmail.com>
2019-09-08 12:06:46 -0300

    configurar projeto para rodar no HTML com pacotes pelo package.sjon

--- curso-typescript/introducao/basico.js
+++ curso-typescript/introducao/basico.js
@@ -1,2 +1,5 @@
-var a = 'Teste TS';
+"use strict";
+//executando de maneira manual pela linha de comando: tsc basico.ts
+//instalando npm i -g ts-node no package.json para executar por comando com coder runner
+var a = "Teste TS (WEB)!";
 console.log(a);
--- curso-typescript/introducao/basico.ts
+++ curso-typescript/introducao/basico.ts
@@ -1,4 +1,4 @@
 //executando de maneira manual pela linha de comando: tsc basico.ts
 //instalando npm i -g ts-node no package.json para executar por comando com coder runner
-const a: string = "Teste TS 22222222!";
+const a: string = "Teste TS (WEB)!";
 console.log(a);
