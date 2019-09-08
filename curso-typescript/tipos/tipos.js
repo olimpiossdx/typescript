8aac73754c8b8837d0192443c639393a85e77af0
Olimpio Pimenta <olimpiodx@gmail.com>
2019-09-08 12:19:01 -0300

    atribuindo tipos explicitos

--- curso-typescript/tipos/tipos.js
+++ curso-typescript/tipos/tipos.js
@@ -2,3 +2,17 @@
 //string
 var nome = "joão";
 console.log(nome);
+// numbers
+var idade = 27;
+console.log("idade:", idade);
+idade = 27.5;
+console.log("idade:", idade);
+// boolean
+var possuiHobbies = false;
+console.log("possuiHobbies", possuiHobbies);
+// tipos explícitos
+var minhaIdade;
+minhaIdade = 27;
+console.log("typeof minhaIdade", typeof minhaIdade);
+// minhaIdade = "27";
+console.log("typeof minhaIdade", typeof minhaIdade);
--- curso-typescript/tipos/tipos.ts
+++ curso-typescript/tipos/tipos.ts
@@ -1,13 +1,20 @@
 //string
-let nome = "joão";
+let nome: string = "joão";
 console.log(nome);
 
 // numbers
-let idade = 27;
+let idade: number = 27;
 console.log("idade:", idade);
 idade = 27.5;
 console.log("idade:", idade);
 
 // boolean
-let possuiHobbies = false;
+let possuiHobbies: boolean = false;
 console.log("possuiHobbies", possuiHobbies);
+
+// tipos explícitos
+let minhaIdade: number;
+minhaIdade = 27;
+console.log("typeof minhaIdade", typeof minhaIdade);
+// minhaIdade = "27";
+console.log("typeof minhaIdade", typeof minhaIdade);
