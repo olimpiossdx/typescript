12e42d7fbaa181646ebda93f6e1d17a222ae01d6
Olimpio Pimenta <olimpiodx@gmail.com>
2019-09-08 11:55:05 -0300

    execução de código por coder runnter com pacote

--- curso-typescript/introducao/basico.ts
+++ curso-typescript/introducao/basico.ts
@@ -1,3 +1,4 @@
 //executando de maneira manual pela linha de comando: tsc basico.ts
-const a: string = "Teste TS";
+//instalando npm i -g ts-node no package.json para executar por comando com coder runner
+const a: string = "Teste TS 22222222!";
 console.log(a);
