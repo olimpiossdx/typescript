3df4df7e90996de28bab3f17bd9c61b31448bd42
Olimpio Pimenta <olimpiodx@gmail.com>
2019-09-08 12:16:28 -0300

    inferencia de tipos:number e boolean

--- curso-typescript/tipos/tipos.ts
+++ curso-typescript/tipos/tipos.ts
@@ -1,4 +1,13 @@
 //string
-
 let nome = "joão";
 console.log(nome);
+
+// numbers
+let idade = 27;
+console.log("idade:", idade);
+idade = 27.5;
+console.log("idade:", idade);
+
+// boolean
+let possuiHobbies = false;
+console.log("possuiHobbies", possuiHobbies);
